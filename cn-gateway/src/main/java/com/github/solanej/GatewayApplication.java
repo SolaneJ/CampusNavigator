d5367957f5e6040a439906d89031247ca0ffa6c0
SolaneJ <c1uan@outlook.com>
2025-04-30 23:36:25 +0800

    ➕ add gateway module

---
+++ cn-gateway/src/main/java/com/github/solanej/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.github.solanej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ *
+ * @since 2025/4/30 21:06
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
