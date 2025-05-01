682e5a9480b2d7e0ff2732ca1ae1ee761551d6ca
SolaneJ <c1uan@outlook.com>
2025-05-01 22:17:41 +0800

    ➕ add elementplus, naiveui

--- cn-ui/src/main.js
+++ cn-ui/src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// naive-ui
+import naive from 'naive-ui'
+
+// element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(naive)
+app.use(ElementPlus)
+
+app.mount('#app')
