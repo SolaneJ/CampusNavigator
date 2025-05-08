fc1abf4273ae5be2512b7343e1218de6771d3e68
SolaneJ <c1uan@outlook.com>
2025-05-08 13:52:40 +0800

    🔧 refactor App.vue, index.js

--- cn-ui/src/router/index.js
+++ cn-ui/src/router/index.js
@@ -1,21 +1,16 @@
+import Index from '@/views/Index.vue'
+import NotFound from '@/views/NotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: Index
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    path: '/:pathMatch(.*)*',
+    component: NotFound
   }
 ]
 
